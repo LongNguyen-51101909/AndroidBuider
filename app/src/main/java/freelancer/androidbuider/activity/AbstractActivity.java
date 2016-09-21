a6530a39b1ebd2d53bce0b94edc7bcedf6557e1c
LongNguyen-51101909 <longnguyenqu@gmail.com>
2016-09-21 17:45:48 +0700

    init

---
+++ app/src/main/java/freelancer/androidbuider/activity/AbstractActivity.java
@@ -0,0 +1,7 @@
+package freelancer.androidbuider.activity;
+
+/**
+ * Created by nqlong on 21-Sep-16.
+ */
+abstract class AbstractActivity {
+}
---
+++ app/src/main/java/freelancer/androidbuider/main.java
@@ -0,0 +1,7 @@
+package freelancer.androidbuider;
+
+/**
+ * Created by nqlong on 21-Sep-16.
+ */
+public class main {
+}
