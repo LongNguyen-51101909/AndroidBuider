166f3a2537e7f22dab5ae304757129bcabe8079c
LongNguyen-51101909 <longnguyenqu@gmail.com>
2016-09-22 08:57:31 +0700

    Commit

--- app/src/main/java/freelancer/androidbuider/AndroidBuiderApplication.java
+++
@@ -1,9 +0,0 @@
-package freelancer.androidbuider;
-
-import android.app.Application;
-
-/**
- * Created by nqlong on 22-Sep-16.
- */
-public class AndroidBuiderApplication extends Application {
-}
---
+++ app/src/main/java/freelancer/androidbuider/adapter/AbstractAdapter.java
@@ -0,0 +1,7 @@
+package freelancer.androidbuider.adapter;
+
+/**
+ * Created by nqlong on 22-Sep-16.
+ */
+abstract class AbstractAdapter {
+}
